1794bda1a593443b58694b68f9ddf93cb73bae5a
Karthik-Nakkala <karthinakkala15@gmail.com>
2025-02-10 22:25:07 +0530

    30Q

---
+++ Bitwise_Check if Number is Power of 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    scanf("%d",&n);
+    if(n<=0){
+        printf("No");
+    }
+    else if( ((n)^(n-1))==(2*n-1) )   
+     {
+         printf("Yes");
+     }
+    else{
+        printf("No");
+    }
+    
+    return 0;
+}
